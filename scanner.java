807d479d0d6b0a19d57c9c8ab26fba8722eff18d
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2021-10-12 16:06:25 -0500

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class Main {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter first name: ");
+
+    String firstName = keyboard.nextLine();  // Read user input
+    System.out.println("First name is: " + firstName);  // Output user input
+  }
+}
